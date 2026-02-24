8798f85ea5df7ef74f72652cb1a1ccc9d9f1f109
Anthony Barreiro <248926418+AnthonyDev369@users.noreply.github.com>
2026-02-23 21:21:46 -0500

    Delete .next/dev directory

--- .next/dev/server/app/api/import-product/route.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/import-product/route.js")
-R.c("server/chunks/node_modules_next_81b3340c._.js")
-R.c("server/chunks/node_modules_@supabase_auth-js_dist_module_02e1b12e._.js")
-R.c("server/chunks/node_modules_62644f13._.js")
-R.c("server/chunks/[root-of-the-server]__61e5b44f._.js")
-R.c("server/chunks/_next-internal_server_app_api_import-product_route_actions_0413594d.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/import-product/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/import-product/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__16499b7d._.css",
-  "static/chunks/_829dbc96._.js"
-],
-    source: "dynamic"
-});
